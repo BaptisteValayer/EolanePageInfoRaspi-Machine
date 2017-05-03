f828f11247741c59a4779e68cdb611fa2a290273
VIALLETON Nicolas <nvn@eolane.com>
2017-05-03 15:16:08 +0200

    creation des diagrammes probleme avec les filltext(...) qui prennent tous le texte du dernier

--- js/file.js
+++ js/file.js
@@ -23,7 +23,9 @@
     console.log(data);
   })
 });*/
+
 var tableauTotale = [];
+
 function getInfo(){
   var MachineNameList = $("h3");
   var MachineCaracList = $("tbody .DataTable").first().children();
@@ -33,7 +35,7 @@ function getInfo(){
         obj[MachineCaracList[0].textContent]=MachineDonneeList[0+(index*8)].textContent,
         obj[MachineCaracList[1].textContent] =MachineDonneeList[1+(index*8)].textContent,
         obj[MachineCaracList[2].textContent+"C"] =MachineDonneeList[2+(index*8)].textContent,
-        obj[MachineCaracList[2].textContent+"P"] =MachineDonneeList[3+(index*8)].textContent,
+        obj[MachineCaracList[2].textContent+"P"] =MachineDonneeList[3+(index*8)].textContent.split(".")[0],
         obj[MachineCaracList[3].textContent+"C"] =MachineDonneeList[4+(index*8)].textContent,
         obj[MachineCaracList[3].textContent+"P"] =MachineDonneeList[5+(index*8)].textContent,
         obj[MachineCaracList[4].textContent] =MachineDonneeList[6+(index*8)].textContent,
@@ -41,5 +43,86 @@ function getInfo(){
         tableauTotale[MachineNameList[index].textContent]=obj;
         obj={};
   });
-  location.href="index.php";//+tableauTotale;
+  console.log(tableauTotale);
+  createPageWithCharts(tableauTotale);
+}
+
+function createPageWithCharts(tableauInfo){
+  $("#allInfo").empty();
+  for (var nomMachine in tableauInfo) {
+    if(tableauInfo[nomMachine]['Temps d\'activitéP']=="-"){
+      $('<canvas>').addClass(nomMachine).attr("width",300).attr("height",300).appendTo("#allCanvas");
+      var ctx = $("."+nomMachine);
+      ctx.drawText({
+        text: nomMachine,
+        fontFamily: 'Arial, Helvetica, Sans-Serif',
+        fontStyle: 'bold',
+        fontSize: 30,
+        x: 150, y: 20,
+        fillStyle: '#1C2833',
+      });
+      ctx.drawRect({
+        fillStyle: '#666666',
+        x: 140, y: 145,
+        fromCenter: false,
+        width: 25,
+        height: 10
+      });
+    }
+    else {
+      $('<canvas>').addClass(nomMachine).attr("width",300).attr("height",300).css("display","inline").css("margin","5").appendTo("#allCanvas");
+      var ctx = $("."+nomMachine);
+      var data = {
+        labels: [
+            "Utilise",
+            "Non Utilise",
+        ],
+        datasets: [
+            {
+                data: [tableauInfo[nomMachine]['Temps d\'activitéP'], 100-tableauInfo[nomMachine]['Temps d\'activitéP']],
+                backgroundColor: [
+                    "#2ECC71",
+                    "#E74C3C",
+                ],
+                hoverBackgroundColor: [
+                    "#2ECC71",
+                    "#E74C3C",
+                ]
+            }]
+      };
+      var myPieChart = new Chart(ctx,{
+        type: 'pie',
+        data: data,
+        options: {
+            title: {
+                display: true,
+                fontSize : 30,
+                fontColor: '#1C2833',
+                text: nomMachine
+            },
+            showTooltips: false,
+            animation:{
+                animateScale:true,
+                onComplete: function () {
+                  this.chart.ctx.textBaseline = "middle";
+                  this.chart.ctx.fillStyle = "#666666";
+                  this.chart.ctx.fillText(tableauInfo[nomMachine]['Temps d\'activitéP']+"%",125,220);
+                  this.chart.ctx.fillText("Pieces = "+tableauInfo[nomMachine]['Pièces'],15,60);
+                  this.chart.ctx.fillText("Cartes = "+tableauInfo[nomMachine]['Cartes unitaires'],15,88);
+                }
+            },
+            legend: {
+              display: false,
+            },
+            tooltips: {
+              enable: false
+            },
+            rotation: 1 * Math.PI,
+            circumference: 1 * Math.PI,
+            cutoutPercentage: 50,
+            responsive: false
+        }
+      });
+    }
+  }
 }
